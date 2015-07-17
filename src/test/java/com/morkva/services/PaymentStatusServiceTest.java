f10e62f8c2531a51691101797348174efa1a1beb
Vlad Averyanov <avevladyslav@gmail.com>
2015-07-17 23:53:01 +0300

    paymentstatus service tested (mocked)

---
+++ src/test/java/com/morkva/services/PaymentStatusServiceTest.java
@@ -0,0 +1,35 @@
+package com.morkva.services;
+
+import com.morkva.entities.PaymentStatus;
+import com.morkva.model.dao.PaymentStatusDao;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+
+public class PaymentStatusServiceTest {
+
+    @Mock
+    PaymentStatusDao paymentStatusDao;
+
+    @InjectMocks
+    PaymentStatusService paymentStatusService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetById() throws Exception {
+        Integer id = 1;
+        PaymentStatus mock = new PaymentStatus();
+        Mockito.when(paymentStatusDao.getById(id)).thenReturn(mock);
+        Assert.assertEquals(mock, paymentStatusDao.getById(id));
+    }
+}
