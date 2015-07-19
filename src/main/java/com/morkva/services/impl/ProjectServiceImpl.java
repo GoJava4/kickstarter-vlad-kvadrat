2109cf2e0fd6a00f66f7cb6500d9fd4acabbe73b
Vladislav Korostel <korostel.v.s@gmail.com>
2015-07-20 00:25:24 +0300

    refactoring xml context

--- src/main/java/com/morkva/services/impl/ProjectServiceImpl.java
+++ src/main/java/com/morkva/services/impl/ProjectServiceImpl.java
@@ -71,7 +71,6 @@ public class ProjectServiceImpl implements ProjectService {
         project.setCurrentMoney(project.getCurrentMoney() + amount.intValue());
         projectDAO.update(project);
 
-
         return project;
     }
 
